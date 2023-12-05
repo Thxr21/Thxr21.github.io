bc026f8dc5d4972a447bd2a5ba6a13355681a15b
MedABada <medbada.se@gmail.com>
2023-12-05 02:01:23 +0100

    v0.2.3

--- Scripts/ContactScript.js
+++ Scripts/ContactScript.js
@@ -1,2 +1,20 @@
-function formVerification() {
+function formVerification() 
+{
+    userName = document.getElementById("name").value;
+
+    if (!verifName(userName)) 
+    {
+        alert("Unavailable name.");  
+        return false;  
+    }
+}
+
+function verifName(ch) 
+{
+    i = 0;
+    while ( i < ch.length && ( (ch[i].toUpperCase()>="A" && ch[i].toUpperCase()<="Z" ) || ( i!= 0 && ch[i] == " " && ch[i+1] != " " && ch[i-1] != " ") ) ) {
+        i++;
+    }
+
+    return i == ch.length && ch != "";
 }
